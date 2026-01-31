08cef9c4221e5d4f7dd03625688a257102e2fb73
ruzhenz <ruzhenzrose@gmail.com>
2026-01-31 13:27:42 +0800

    Revert "Role system"

--- lifee/config/__init__.py
+++
@@ -1,4 +0,0 @@
-"""配置模块"""
-from .settings import Settings, settings
-
-__all__ = ["Settings", "settings"]
--- lifee/sessions/__init__.py
+++
@@ -1,5 +0,0 @@
-"""会话管理模块"""
-from .session import Session
-from .store import SessionStore
-
-__all__ = ["Session", "SessionStore"]
